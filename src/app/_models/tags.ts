0f2e502e54fee3bbe6e099a2779ca32d9972718e
Dulan Jayawickrama <jayawickramadulan@gmail.com>
2024-03-04 01:16:55 +0530

    Update tags.ts

--- src/app/_models/tags.ts
+++ src/app/_models/tags.ts
@@ -2,9 +2,11 @@ export class Tag {
   static readonly ANGULAR = new Tag('Angular');
   static readonly TYPESCRIPT = new Tag('TypeScript');
   static readonly JAVA = new Tag('Java');
-  static readonly JAVAFX = new Tag('Angular');
-  static readonly JAVASCRIPT = new Tag('TypeScript');
-  static readonly MYSQL = new Tag('Java');
+  static readonly JAVAFX = new Tag('JavaFX');
+  static readonly JAVASCRIPT = new Tag('JavaScript');
+  static readonly MYSQL = new Tag('MySQL');
+  static readonly NODEJS = new Tag('Node.js');
+  static readonly EXPRESSJS = new Tag('Express.js');
 
   private constructor(private readonly key: string) {}
 
